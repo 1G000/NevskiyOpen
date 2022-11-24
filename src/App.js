5318edb2a43cb292d35c7b178fed64be72889023
1G000 <gorbatovskiy.igor@gmail.com>
2022-11-24 21:01:59 +0300

    Тренируюсь отображать содержимое объектов на странице с помощью map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
+import React from "react"
 import "./App.css"
-import Cards from "./components/Cards"
+// import Cards from "./components/Cards"
+
+const players = [
+  {
+    imageUrl: "./img/1.jpg",
+    name: "Victor",
+    secondName: "Razmarin",
+    age: 30,
+    status: "active",
+  },
+  {
+    imageUrl: "./img/2.jpg",
+    name: "Igor",
+    secondName: "GRB",
+    age: 32,
+    status: "legend",
+  },
+  {
+    imageUrl: "./img/3.jpg",
+    name: "Ivan",
+    secondName: "Ivanov",
+    age: 32,
+    status: "bot",
+  },
+]
 
 function App() {
-  return (
-    <div className="App">
-      <Cards />
+  return players.map((player) => (
+    <div>
+      <img src={player.imageUrl} width={150} height={170} alt="blazergreen" />
+      <h5>{`Имя: ${player.name}`}</h5>
+      <h5>{`Фамилия: ${player.secondName}`}</h5>
+      <div>
+        <div className="mb-40 d-flex flex-column">
+          <span>{player.age}</span>
+          <b>{player.status}</b>
+        </div>
+      </div>
     </div>
-  )
+  ))
 }
 
 export default App
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -29,4 +29,4 @@ function Card() {
   )
 }
 
-export default Card
+// export default Card
