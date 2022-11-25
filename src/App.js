2964aa629f6cbb3636db4c55db45bc6752f6b5dc
1G000 <gorbatovskiy.igor@gmail.com>
2022-11-25 23:56:08 +0300

    Начал творить историю

--- src/App.js
+++ src/App.js
@@ -24,22 +24,103 @@ const players = [
     age: 32,
     status: "bot",
   },
+  {
+    imageUrl: "./img/1.jpg",
+    name: "Victor",
+    secondName: "Razmarin",
+    age: 30,
+    status: "active",
+  },
+  {
+    imageUrl: "./img/2.jpg",
+    name: "Igor",
+    secondName: "GRB",
+    age: 32,
+    status: "legend",
+  },
+  {
+    imageUrl: "./img/3.jpg",
+    name: "Ivan",
+    secondName: "Slabakov",
+    age: 32,
+    status: "bot",
+  },
+  {
+    imageUrl: "./img/2.jpg",
+    name: "Igor",
+    secondName: "GRB",
+    age: 32,
+    status: "legend",
+  },
+  {
+    imageUrl: "./img/3.jpg",
+    name: "Ivan",
+    secondName: "Ivanov",
+    age: 32,
+    status: "bot",
+  },
 ]
 
 function App() {
-  return players.map((player) => (
+  return (
     <div>
-      <img src={player.imageUrl} width={150} height={170} alt="blazergreen" />
-      <h5>{`Имя: ${player.name}`}</h5>
-      <h5>{`Фамилия: ${player.secondName}`}</h5>
-      <div>
-        <div className="mb-40 d-flex flex-column">
-          <span>{player.age}</span>
-          <b>{player.status}</b>
+      <div className="wrapper">
+        <div className="header d-flex justify-center">
+          <h1>Стэк игроков</h1>
+        </div>
+        <div className="cards d-flex justify-between flex-wrap">
+          {players.map((player) => (
+            <div className="card d-flex flex-column align-center">
+              <img
+                src={player.imageUrl}
+                width={150}
+                height={170}
+                alt="blazergreen"
+              />
+              <span>{`Имя: ${player.name}`}</span>
+              <span>{`Фамилия: ${player.secondName}`}</span>
+              <span> {`Возраст: ${player.age} лет`}</span>
+              <span>{`Статус: ${player.status}`}</span>
+            </div>
+          ))}
+        </div>
+        <div className="table d-flex flex-column align-center">
+          <h3>Рейтинг</h3>
+          <table>
+            <tr>
+              <th>№</th>
+              <th>Фио</th>
+              <th>Победы</th>
+              <th>Поражения</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Виктор Размарин</td>
+              <td>3</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Игорь ГРБ</td>
+              <td>2</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Иван Слабаков</td>
+              <td>0</td>
+              <td>3</td>
+            </tr>
+          </table>
+        </div>
+        <div className="results d-flex flex-column align-center">
+          <h3>Результаты матчей</h3>
+          <span>Игорь ГРБ VS Иван Слабаков 6-1,6-2 </span>
+          <span>Игорь ГРБ VS Виктор Размарин 4-6,6-3,5-7 </span>
         </div>
       </div>
     </div>
-  ))
+  )
 }
 
 export default App
