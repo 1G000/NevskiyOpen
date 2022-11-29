70b120221cf071ea45fcf87cc99d2db70f4b7a7d
1G000 <gorbatovskiy.igor@gmail.com>
2022-11-29 22:34:48 +0300

    начал в компоненты

--- src/App.js
+++ src/App.js
@@ -1,270 +1,45 @@
 import React from "react"
 import "./App.scss"
-// import Cards from "./components/Cards"
+import Card from "./components/Card"
+import Rank from "./components/Rank/Rank"
+import Results from "./components/Results/Results"
 
-const players = [
-  {
-    imageUrl: "./img/1.jpg",
-    name: "Victor",
-    secondName: "Razmarin",
-    age: 30,
-    status: "active",
-  },
-  {
-    imageUrl: "./img/2.jpg",
-    name: "Igor",
-    secondName: "GRB",
-    age: 32,
-    status: "legend",
-  },
-  {
-    imageUrl: "./img/3.jpg",
-    name: "Ivan",
-    secondName: "Ivanov",
-    age: 32,
-    status: "bot",
-  },
-  {
-    imageUrl: "./img/1.jpg",
-    name: "Victor",
-    secondName: "Razmarin",
-    age: 30,
-    status: "active",
-  },
-  {
-    imageUrl: "./img/2.jpg",
-    name: "Igor",
-    secondName: "GRB",
-    age: 32,
-    status: "legend",
-  },
-  {
-    imageUrl: "./img/3.jpg",
-    name: "Ivan",
-    secondName: "Slabakov",
-    age: 32,
-    status: "bot",
-  },
-  {
-    imageUrl: "./img/2.jpg",
-    name: "Igor",
-    secondName: "GRB",
-    age: 32,
-    status: "legend",
-  },
-  {
-    imageUrl: "./img/3.jpg",
-    name: "Ivan",
-    secondName: "Ivanov",
-    age: 32,
-    status: "bot",
-  },
-]
+// const players = [
+//   {
+//     imageUrl: "./img/1.jpg",
+//     name: "Victor",
+//     secondName: "Razmarin",
+//     age: 30,
+//     status: "active",
+//   },
+//   {
+//     imageUrl: "./img/2.jpg",
+//     name: "Igor",
+//     secondName: "GRB",
+//     age: 32,
+//     status: "legend",
+//   },
+//   {
+//     imageUrl: "./img/3.jpg",
+//     name: "Ivan",
+//     secondName: "Ivanov",
+//     age: 32,
+//     status: "bot",
+//   },
+// ]
 
 function App() {
   return (
     <div>
       <div className="wrapper">
         <div className="header d-flex justify-center">
-          <h1>Стэк игроков</h1>
+          <h1>Ростер игроков</h1>
         </div>
         <div className="cards d-flex justify-between flex-wrap">
-          {players.map((player) => (
-            <div className="card d-flex flex-column align-center">
-              <img
-                src={player.imageUrl}
-                width={150}
-                height={170}
-                alt="blazergreen"
-              />
-              <span>{`Имя: ${player.name}`}</span>
-              <span>{`Фамилия: ${player.secondName}`}</span>
-              <span> {`Возраст: ${player.age} лет`}</span>
-              <span>{`Статус: ${player.status}`}</span>
-            </div>
-          ))}
+          <Card />
         </div>
-        <div className="table d-flex flex-column align-center">
-          <h3>Рейтинг</h3>
-          <table>
-            <tr>
-              <th>№</th>
-              <th>Фото</th>
-              <th>Фио</th>
-              <th>Победы</th>
-              <th>Поражения</th>
-              <th>Разница по сетам в/п</th>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td></td>
-              <td>Женя</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td></td>
-              <td>Саша врач</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td></td>
-              <td>Кирилл</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td></td>
-              <td>Данила Пыленок</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td></td>
-              <td>Саша американец</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>6</td>
-              <td></td>
-              <td>Артём</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>7</td>
-              <td></td>
-              <td>Пётр</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>8</td>
-              <td>
-                {/* <img width={50} height={60} src={"./img/2.jpg"}></img> */}
-              </td>
-              <td>Игорь Горбатовский</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>9</td>
-              <td></td>
-              <td>Григор</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>10</td>
-              <td></td>
-              <td>Сергей</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>11</td>
-              <td></td>
-              <td>Андрей</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>12</td>
-              <td></td>
-              <td>Антон</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>13</td>
-              <td></td>
-              <td>Саша</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>14</td>
-              <td></td>
-              <td>Яков</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>15</td>
-              <td></td>
-              <td>Сергей М</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>16</td>
-              <td></td>
-              <td>Максим</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>17</td>
-              <td></td>
-              <td>Евгений</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>18</td>
-              <td></td>
-              <td>Костя</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>19</td>
-              <td></td>
-              <td>Саша музыкант</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>20</td>
-              <td></td>
-              <td>Даня</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </table>
-        </div>
-        <div className="results d-flex flex-column align-center">
-          <h3>Результаты матчей</h3>
-          <span>Игорь ГРБ VS Иван Слабаков 6-1,6-2 </span>
-          <span>Игорь ГРБ VS Виктор Размарин 4-6,6-3,5-7 </span>
-          <span>Иван Слабаков VS Виктор Размарин 1-6,2-6 </span>
-        </div>
+        <Rank />
+        <Results />
       </div>
     </div>
   )
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,29 +4,22 @@ import cardStyles from "./Card.module.scss"
 function Card() {
   return (
     <div className={cardStyles.card}>
-      <div className={cardStyles.favorite}>
-        <img src={"/img/heart-liked.svg"} alt="Unliked" />
-      </div>
-      <img
-        src={"/img/PlayersPhoto/GorbatovskiyIgor.jpg"}
-        width={150}
-        height={170}
-        alt="blazergreen"
-      />
-      <h5>Горбатовский Игорь</h5>
-      <div className="d-flex justify-between align-center">
-        <div className="d-flex flex-column">
-          <span>Гений игры</span>
-          <b>Красава</b>
-        </div>
-        <img
-          className={cardStyles.plus}
-          src={"/img/btn-plus.svg"}
-          alt="plus"
-        ></img>
-      </div>
+      <img src={"./img/3.jpg"} width={150} height={170} alt="blazergreen" />
+      <span>{`Имя: Игорь`}</span>
+      <span> {`Возраст: 30 лет`}</span>
+      <span>{`Статус: Легенда`}</span>
     </div>
   )
 }
 
-// export default Card
+export default Card
+
+// return (
+//   <div className="card d-flex flex-column align-center">
+//     <img src={player.imageUrl} width={150} height={170} alt="blazergreen" />
+//     <span>{`Имя: ${player.name}`}</span>
+//     <span>{`Фамилия: ${player.secondName}`}</span>
+//     <span> {`Возраст: ${player.age} лет`}</span>
+//     <span>{`Статус: ${player.status}`}</span>
+//   </div>
+// )
