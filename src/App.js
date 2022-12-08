2730576f798262e91763c3101c1b2a48f606580a
1G000 <gorbatovskiy.igor@gmail.com>
2022-12-08 21:20:30 +0300

    неудачно попробовал подключить react router, ничего не работает, завтра разберусь

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./App.scss"
 import Card from "./components/Card"
 import Rank from "./components/Rank"
@@ -7,35 +8,48 @@ import players from "./components/Data/Data"
 
 function App() {
   return (
-    <div>
-      <div className="wrapper">
-        <div className="header d-flex justify-center">
-          <h1>Ростер игроков</h1>
-        </div>
-        <div className="cards d-flex justify-between flex-wrap">
-          {players.map((obj) => (
-            <Card
-              name={obj.name}
-              secondName={obj.secondName}
-              age={obj.age}
-              status={obj.status}
-              imageUrl={obj.imageUrl}
-              birthDate={obj.birthDate}
-              turnToProDate={obj.turnToProDate}
-              powerHand={obj.powerHand}
-              backhandStyle={obj.backhandStyle}
-              rankingPosition={obj.rankingPosition}
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <div className="wrapper">
+            <div className="header d-flex justify-center">
+              <h1>Ростер игроков</h1>
+            </div>
+            <Route
+              path="/"
+              element={
+                <div className="cards d-flex justify-between flex-wrap">
+                  {players.map((obj) => (
+                    <Card
+                      name={obj.name}
+                      secondName={obj.secondName}
+                      age={obj.age}
+                      status={obj.status}
+                      imageUrl={obj.imageUrl}
+                      birthDate={obj.birthDate}
+                      turnToProDate={obj.turnToProDate}
+                      powerHand={obj.powerHand}
+                      backhandStyle={obj.backhandStyle}
+                      rankingPosition={obj.rankingPosition}
+                    />
+                  ))}
+                </div>
+              }
             />
-          ))}
-        </div>
-
-        <Rank />
-        <div className="d-flex align-center flex-column">
-          <h3>Результаты матчей</h3>
-          <Results />
-        </div>
+            <Route path="ranked" element={<Rank />} />
+            <Route
+              path="results"
+              element={
+                <div className="d-flex align-center flex-column">
+                  <h3>Результаты матчей</h3>
+                  <Results />
+                </div>
+              }
+            />
+          </div>
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
