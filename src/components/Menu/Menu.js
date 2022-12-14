c42a99dd18bb9df27f9aab6f831057f34b185ce4
1G000 <gorbatovskiy.igor@gmail.com>
2022-12-14 22:36:53 +0300

    верстаю адаптивный navBar

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,15 +1,27 @@
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
+import navBarStyles from "../Menu/Menu.module.scss"
 
 const Menu = () => {
+  const [showLinks, setShowLinks] = useState(false)
   return (
-    <nav>
-      <div className="container">
-        <NavLink to="/" end>
-          Главная
-        </NavLink>
-        <NavLink to="/roster">Ростер</NavLink>
-        <NavLink to="/rating">Рейтинг</NavLink>
-        <NavLink to="/results">Результаты</NavLink>
+    <nav className={navBarStyles.NavBar}>
+      <div className={navBarStyles.leftSide}>
+        <div className={navBarStyles.links} id={showLinks ? "hidden" : ""}>
+          <NavLink to="/" end>
+            Главная
+          </NavLink>
+          <NavLink to="/roster">Ростер</NavLink>
+          <NavLink to="/rating">Рейтинг</NavLink>
+          <NavLink to="/results">Результаты</NavLink>
+          <NavLink to="/results">Турниры</NavLink>
+          <NavLink to="/results">Галерея</NavLink>
+          <NavLink to="/results">О нас</NavLink>
+        </div>
+        <button>Open</button>
+      </div>
+      <div className={navBarStyles.rightSide}>
+        <h2>Невская лига</h2>
       </div>
     </nav>
   )
